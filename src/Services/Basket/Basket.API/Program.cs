0461d26242fc4c9c0ec8ec0939109adfa23d38bc
stefank1995 <stefan.kolaric4@gmail.com>
2023-05-09 14:17:06 +0200

    Implemented Repository for Redis to BasketAPI

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,7 +1,3 @@
-using Catalog.API.Data;
-using Catalog.API.Repositories;
-
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -10,9 +6,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<ICatalogContext, CatalogContext>();
-builder.Services.AddScoped<IProductRepository, ProductRepository>();
-
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetValue<String>("CacheSettings:ConnectionString");
+});
 
 var app = builder.Build();
 
