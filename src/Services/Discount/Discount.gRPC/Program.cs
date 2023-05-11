e183fbd00230cb8bfe3eaf6c7ec7c5b7e6960523
stefank1995 <stefan.kolaric4@gmail.com>
2023-05-11 15:44:52 +0200

    Added Postgres Connection and Implemented Repository pattern to Discount.gRPC

--- src/Services/Discount/Discount.gRPC/Program.cs
+++ src/Services/Discount/Discount.gRPC/Program.cs
@@ -1,3 +1,5 @@
+using Discount.gRPC.Extensions;
+using Discount.gRPC.Repositories;
 using Discount.gRPC.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc();
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
 
 var app = builder.Build();
 
@@ -14,4 +17,5 @@ var app = builder.Build();
 app.MapGrpcService<GreeterService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
+app.MigrateDatabase<Program>();
 app.Run();
