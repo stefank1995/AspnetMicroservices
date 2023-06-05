31c8759f4322202f2071d0dd0b91afdc2eb9503f
stefank1995 <stefan.kolaric4@gmail.com>
2023-06-05 18:06:00 +0200

    Created and set up the Ocelot Gateway json files - created routing for the gateway

--- src/ApiGateways/OcelotApiGateway/Program.cs
+++ src/ApiGateways/OcelotApiGateway/Program.cs
@@ -8,6 +8,11 @@ var builder = WebApplication.CreateBuilder(args);
 //builder.Logging.AddConsole();
 //builder.Logging.AddDebug();
 
+
+builder.Host.ConfigureAppConfiguration((hostingContext, config) =>
+{
+    config.AddJsonFile($"ocelot.{hostingContext.HostingEnvironment}.json", true, true);
+});
 //.net6
 builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
 builder.Logging.AddConsole();
@@ -19,8 +24,5 @@ builder.Services.AddOcelot();
 var app = builder.Build();
 
 await app.UseOcelot();
-
-
-app.MapGet("/", () => "Hello World!");
 
 app.Run();
